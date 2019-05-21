81df34705d6f3bb325ae295458ac7892198b5143
David Braley <david.braley@gmail.com>
2019-05-21 12:26:40 -0400

    Working test for pressure too low

--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
@@ -2,13 +2,36 @@ package tddmicroexercises.tirepressuremonitoringsystem;
 
 
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
 import static org.junit.Assert.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TestAlarm {
 
+    @Mock
+    Sensor sensor;
+
+    @InjectMocks
+    Alarm alarm;
+
     @Test
-    public void foo() {
+    public void testAlarmInitializedToFalse() {
         Alarm alarm = new Alarm();
         assertEquals(false, alarm.isAlarmOn());
     }
+
+    @Test
+    public void testAlarmOnWhenPressureTooLow() {
+        Mockito.when(sensor.popNextPressurePsiValue()).thenReturn((double) 5);
+
+        alarm.check();
+        assertEquals(true, alarm.isAlarmOn());
+    }
+
+
 }
